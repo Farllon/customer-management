72544363d57fa5bd067ecb04ebfd3da106d88842
Farllon Augusto <farllon.costa@conferironline.com.br>
2022-07-02 07:57:39 -0300

    :rocket: fetaure: Firsst commit

---
+++ src/Course.Customers.API/Controllers/CustomersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Course.Customers.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetById()
+        {
+            return Ok("Testando");
+        }
+    }
+}
